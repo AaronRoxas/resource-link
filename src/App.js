058d1ca3294ab1777e786ba42e1c074c8b763e0d
aaronroxas <aaron.roxas@lpunetwork.edu.ph>
2024-10-19 23:41:48 +0800

    Resource Link project creation Splash page and login page user interface created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './styles/App.css';
+import Splash from './Splash.js';
+import LoginComponent from './LoginComponent.js';
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showSplash ? <Splash /> : <LoginComponent />}
     </div>
   );
 }
