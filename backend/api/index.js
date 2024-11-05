0b48aa3460150d570db7a2a8da406a5ec9b65860
aaronroxas <aaron.roxas@lpunetwork.edu.ph>
2024-11-05 19:51:32 +0800

    Refactor CORS configuration to allow requests from multiple origins and methods

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,9 +5,13 @@ const authRoutes = require('../routes/auth'); // Ensure this path is correct
 const connectDB = require('../config/db'); // Import the connectDB function
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://resource-link.vercel.app/", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json()); // Parse JSON request bodies
-
+app.options('*', cors()); // Enable preflight for all routes
 // Connect to the database
 connectDB()
   .catch(err => console.error('Database connection error:', err));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
+  console.log('Username:', username);
+  console.log('Password:', password);
 
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: 'Invalid username or password' });
+      return res.status(404).json({ message: 'User not found' });
     }
 
-    const isMatch = await user.comparePassword(password);
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log('Password Match:', isMatch);
+
     if (!isMatch) {
       return res.status(401).json({ message: 'Invalid username or password' });
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ token, role: user.role });
+    res.status(200).json({ message: 'Login successful' });
   } catch (error) {
     console.error('Login error:', error);
-    res.status(500).json({ message: 'Internal Server Error', error: error.message });
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
