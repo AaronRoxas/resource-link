f6b22925ef40eb19754fabb8d7543ae03ec1fc27
aaronroxas <aaron.roxas@lpunetwork.edu.ph>
2024-11-05 21:11:03 +0800

    Add CORS headers to allow cross-origin requests

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,6 +10,13 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 app.use(express.json()); // Parse JSON request bodies
 app.options('*', cors()); // Enable preflight for all routes
 // Connect to the database
@@ -28,6 +35,8 @@ app.use('/api/auth', authRoutes);
 app.use((req, res) => {
   res.status(404).send('Route not found');
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
