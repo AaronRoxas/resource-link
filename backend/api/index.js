4039eac125dfbb9c8db98c6d201badbf74eab25b
aaronroxas <aaron.roxas@lpunetwork.edu.ph>
2024-11-30 21:35:14 +0800

    Fix: Update dependencies

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('../routes/auth'); 
 const connectDB = require('../config/db'); 
