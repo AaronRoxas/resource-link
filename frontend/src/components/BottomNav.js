bfeb91ffb1928932bb538bc3ec6b8c33f2faea49
aaronroxas <aaron.roxas@lpunetwork.edu.ph>
2024-12-01 08:45:48 +0800

    Subcategory and item adding for admin

--- frontend/src/components/BottomNav.js
+++ frontend/src/components/BottomNav.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import '../styles/BottomNav.css';
-const BottomNav = ({ navItems, setShowQRScanner }) => {
-  const navigate = useNavigate(); 
-  const location = useLocation(); 
 
+const BottomNav = ({ navItems }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const handleNavigation = (path) => {
-    if (path === '/qr-scan') {
-      setShowQRScanner(true);
-    } else {
-      navigate(path); 
-    }
+    navigate(path);
   };
 
   return (
-    <nav className="bottom-nav">
-      {navItems.map((item) => (
-        <div
-          key={item.path}
-          className={`${item.icon}-icon ${location.pathname === item.path ? 'active' : ''} ${item.label === 'QR Code' ? 'qr-icon' : ''}`} 
-          onClick={() => handleNavigation(item.path)}
-        >
-          <img src={`/footer-imgs/${item.icon}.svg`} alt={item.label} />
-          <span>{item.label}</span>
-        </div>
-      ))}
-    </nav>
+    <div className="bottom-nav-container">
+      <nav className="bottom-nav">
+        {navItems.map((item) => (
+          <div
+            key={item.path}
+            className={`nav-item ${location.pathname === item.path ? 'active' : ''}`}
+            onClick={() => handleNavigation(item.path)}
+          >
+            <img src={`/footer-imgs/${item.icon}.svg`} alt={item.label} />
+            <span>{item.label}</span>
+          </div>
+        ))}
+      </nav>
+    </div>
   );
 };
 
